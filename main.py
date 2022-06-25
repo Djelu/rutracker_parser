1447e35ad437037225c0bff202bb7e5a2d67a47a
izhukov <izhukov@btlab.ru>
2022-06-26 00:15:14 +0300

    fix

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def main():
     try:
         # ids = db_service.get_ids()
         parser = rutracker.Parser()
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
         asyncio.run(parser.run())
     except Exception as ex:
         print(ex)
